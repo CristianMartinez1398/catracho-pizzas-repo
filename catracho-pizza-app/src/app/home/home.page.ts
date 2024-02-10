41df7a713b1ddff3e3fbca0195ebbb42b46dbdb3
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2024-02-10 02:18:44 -0600

    diseño y programación
    Diseñar la tarjeta y la funcionalidad de los botones de mas y menos.

--- catracho-pizza-app/src/app/home/home.page.ts
+++ catracho-pizza-app/src/app/home/home.page.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonCardSubtitle } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardContent, IonBackButton, IonButtons, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.css'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardContent, IonButtons, IonInput],
 })
 export class HomePage {
+
+  precio: number = 329.00
+
+  numero: number = 0;
+  resta(): void{
+    if (this.numero > 0) {
+      this.numero--;
+    }
+    
+  }
+  sum(): void{
+    this.numero++;
+  }
+
   constructor() {}
 }
