7b317b4d576e64957d8e3175edde9eb8a6c9741f
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2024-02-11 21:28:09 -0600

    las alertas de orden de compra

--- catracho-pizza-app/src/app/home/home.page.ts
+++ catracho-pizza-app/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonCardSubtitle } from '@ionic/angular';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardContent, IonBackButton, IonButtons, IonInput, IonList, IonItem } from '@ionic/angular/standalone';
+import { IonCardSubtitle, AlertController } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardContent, IonBackButton, IonButtons, IonInput, IonList, IonItem, IonAlert } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +8,7 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, Ion
   styleUrls: ['home.page.css'],
   
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardContent, IonButtons, IonInput, IonList, IonItem],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardContent, IonButtons, IonInput, IonList, IonItem, IonAlert],
 })
 export class HomePage {
 
@@ -25,5 +25,25 @@ export class HomePage {
     this.numero++;
   }
 
-  constructor() {}
+
+
+  constructor(public alertController: AlertController) {}
+  async mostrarAlerta(){
+    /* En este método es para mandar un mensaje cuando el usuario compre si el producto es mayor que 0
+       entonces la alerta se activa si es 0 no se activa el mensaje.*/
+    if(this.numero > 0){
+      const alert = await this.alertController.create({
+        header:'Producto Agregado',
+        message: 'Ha agregado correctamente el producto a su carrito',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+    }
+   
+
+   
+
+  }
+
 }
